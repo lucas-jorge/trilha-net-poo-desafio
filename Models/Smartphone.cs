8372ff933b0ba7b2cb7054d1477c54308e0aeeef
lucas-jorge <lucasajorge@gmail.com>
2023-12-13 10:50:17 -0300

    Desafio POO my implementation

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,26 +1,26 @@
 namespace DesafioPOO.Models
 {
-    public abstract class Smartphone
+using System;
+
+// Classe abstrata Smartphone
+public abstract class Smartphone
+{
+    public string Numero { get; set; }
+    protected string Modelo { get; set; }
+    protected string IMEI { get; set; }
+    protected int Memoria { get; set; }
+
+    public void Ligar()
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
-        {
-            Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
-        }
-
-        public void Ligar()
-        {
-            Console.WriteLine("Ligando...");
-        }
-
-        public void ReceberLigacao()
-        {
-            Console.WriteLine("Recebendo ligação...");
-        }
-
-        public abstract void InstalarAplicativo(string nomeApp);
+        Console.WriteLine("Realizando ligação...");
     }
+
+    public void ReceberLigacao()
+    {
+        Console.WriteLine("Recebendo ligação...");
+    }
+
+    // Método abstrato para instalar aplicativos
+    public abstract void InstalarAplicativo(string nome);
+}
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+class Program
+{
+    static void Main()
+    {
+        // Instanciação direta de objetos das classes derivadas é possível
+        // mas a classe abstrata Smartphone não pode ser instanciada.
+
+        Nokia nokia = new Nokia();
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Iphone iphone = new Iphone();
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+
+    }
+}
